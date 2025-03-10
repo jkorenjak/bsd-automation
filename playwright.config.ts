c3593a65a46deececa8ae09bfc9dab9b40580f14
Jon Korenjak <jon.korenjak98@gmail.com>
2025-03-10 13:47:26 +0100

    Complete workflow rework
    Signed-off-by: Jon Korenjak <jon.korenjak98@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,12 @@ import {PlaywrightTestConfig} from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
     testDir: './tests',
+    outputDir: '.test-results',
     timeout: 30000,
-    reporter: 'html',
+    reporter: [
+        ['html', {outputFolder: 'playwright-report'}],
+        ['list']
+    ],
     workers: 1,
     use: {
         baseURL: process.env.BASE_URL || 'http://localhost:3000',
